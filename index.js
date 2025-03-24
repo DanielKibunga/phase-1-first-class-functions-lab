fd55a4e0055cdfbdcff02ff1f216fac68a963a40
Daniel Kimani <daniel.kibunga@students.moringaschool.com>
2025-03-24 14:21:05 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => (fare) => fare * multiplier;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers, driverFunction) => driverFunction(arrayOfDrivers);
+
+if (typeof module !== "undefined") {
+    module.exports = {
+        returnFirstTwoDrivers,
+        returnLastTwoDrivers,
+        selectingDrivers,
+        createFareMultiplier,
+        fareDoubler,
+        fareTripler,
+        selectDifferentDrivers
+    };
+}
